24004f3e9d0c914956a3506d9852d81441abd7e1
Alexander Zuliani <alexander.zuliani@tass.be>
2014-11-25 16:43:51 +0100

    libhttps uses PolarSSL for now, hybrid is next

--- libhttps/pico_https_glue.h
+++ libhttps/pico_https_glue.h
@@ -5,10 +5,10 @@
 #include "pico_stack.h"
 #include "pico_tcp.h"
 #include "pico_tree.h"
-#include "cyassl/ssl.h"
+#include "polarssl/ssl.h"
 
-int pico_EmbedSend(CYASSL* ssl, char *buf, int sz, void *ctx);
+int pico_EmbedReceive(void * pico_sock, unsigned char * buf, size_t sz);
+int pico_EmbedSend(void * pico_sock, const unsigned char * buf, size_t sz);
 
-int pico_EmbedReceive(CYASSL *ssl, char *buf, int sz, void *ctx);
 
 #endif
