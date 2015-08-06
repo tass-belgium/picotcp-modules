506d98923b26e856adf9040a07bb6f343c8b0fce
Maarten Arits <maarten_arits@hotmail.com>
2015-08-06 11:52:40 +0200

    add flag to disable DNS lookup, changed names from socket to connection because the interface is connection oriented and can change sockets. Changed the signature of the close function

--- pico_mqtt/pico_mqtt_socket.h
+++ pico_mqtt/pico_mqtt_socket.h
@@ -2,17 +2,25 @@
 #define PICO_MQTT_SOCKET_H
 
 #include <stdint.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+
+// enable dns lookup or only allow IP addresses
+#define PICO_MQTT_DNS_LOOKUP 1
+
+struct pico_mqtt_socket;
 
 // create pico mqtt socket and connect to the URI, returns -1 on error
-int pico_mqtt_socket_open(const char* URI, uint32_t timeout);
+struct pico_mqtt_socket* pico_mqtt_connection_open( const char* URI, uint32_t timeout);
 
 // read data from the socket, return the number of bytes read
-int pico_mqtt_socket_read( int socket_file_descriptor, void* read_buffer, uint16_t count, uint32_t timeout);
+int pico_mqtt_connection_read( struct pico_mqtt_socket* socket, void* read_buffer, uint16_t count, uint32_t timeout);
 
 // write data to the socket, return the number of bytes written
-int pico_mqtt_socket_write( int socket_file_descriptor, void* write_buffer, uint16_t count, uint32_t timeout);
+int pico_mqtt_connection_write( struct pico_mqtt_socket* socket, void* write_buffer, uint16_t count, uint32_t timeout);
 
 // close pico mqtt socket, return -1 on error
-int pico_mqtt_socket_close( int socket_file_descriptor );
+int pico_mqtt_connection_close( struct pico_mqtt_socket* socket );
 
 #endif
