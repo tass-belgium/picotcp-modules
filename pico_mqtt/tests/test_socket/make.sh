303e137a31da5d98904f84a465c5905a16bddb93
Maarten Arits <maarten_arits@hotmail.com>
2015-08-06 11:46:35 +0200

    addapted make script to only use flags, no more lookups

--- pico_mqtt/tests/test_socket/make.sh
+++ pico_mqtt/tests/test_socket/make.sh
@@ -1,2 +1,1 @@
-gcc -Wall -Wextra -Werror -std=c99 -pedantic -o pico_mqtt_socket_test.out pico_mqtt_socket_test.c `pkg-config --cflags --libs check`
-
+gcc -Wall -Wextra -Werror -std=c99 -pedantic -o pico_mqtt_socket_test.out pico_mqtt_socket_test.c -lcheck -lpthread -lm -lrt
