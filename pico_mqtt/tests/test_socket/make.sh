826f3f42ffe9ea7fb634527fa6e204950bddd434
Maarten Arits <maarten_arits@hotmail.com>
2015-08-06 23:43:50 +0200

    make script updated

--- pico_mqtt/tests/test_socket/make.sh
+++ pico_mqtt/tests/test_socket/make.sh
@@ -1,1 +1,3 @@
-gcc -Wall -Wextra -Werror -std=c99 -pedantic -o pico_mqtt_socket_test.out pico_mqtt_socket_test.c -lcheck -lpthread -lm -lrt
+clear
+clear
+gcc -Wall -Wextra -Werror -std=c99 -o pico_mqtt_socket_test.out pico_mqtt_socket_test.c ../../pico_mqtt_socket.c -lcheck -lpthread -lm -lrt 
