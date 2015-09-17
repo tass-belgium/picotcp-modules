7bc6e0faa201cc317b2d767650aab7c7ab315b4c
Maarten Arits <maarten_arits@hotmail.com>
2015-09-17 03:28:36 +0200

    MQTT: added lists to object

--- pico_mqtt/pico_mqtt_private.h
+++ pico_mqtt/pico_mqtt_private.h
@@ -7,34 +7,39 @@
 * Data Types
 **/
 
-struct pico_mqtt{
-	struct pico_mqtt_socket* socket;
-	uint8_t tls_connections;
-	char * URI;
-	struct pico_mqtt* this;
+struct pico_mqtt
+{
+/*        struct pico_mqtt* this;*/
 
-	// status
-	uint8_t connected;
-	char normative_error[15];
-	uint32_t documentation_reference;
-	uint16_t next_packet_id;
+        // stream
+        struct pico_mqtt_stream* stream;
 
-	// serializer
-	struct pico_mqtt_serializer* serializer;
-	uint16_t next_message_id;
+        // serializer
+        struct pico_mqtt_serializer* serializer;
+        uint16_t next_message_id;
 
-	// connection related
-	uint16_t keep_alive_time;
-	struct pico_mqtt_data client_id;
-	struct pico_mqtt_data will_topic;
-	struct pico_mqtt_data will_message;
-	struct pico_mqtt_data user_name;
-	struct pico_mqtt_data password;
+	// message buffers
+	struct pico_mqtt_list* output_queue;
+	struct pico_mqtt_list* active_messages;
 
-	// memory
-	int (*malloc)(struct pico_mqtt*, void**, size_t);
-	int (*free)(struct pico_mqtt*, void*, size_t);
-	uint32_t bytes_used;
+        // connection related
+        uint16_t keep_alive_time;
+        struct pico_mqtt_data* client_id;
+        struct pico_mqtt_data* will_topic;
+        struct pico_mqtt_data* will_message;
+        struct pico_mqtt_data* user_name;
+        struct pico_mqtt_data* password;
+
+        // memory
+        int (*malloc)(struct pico_mqtt*, void**, size_t);
+        int (*free)(struct pico_mqtt*, void*, size_t);
+        uint32_t bytes_used;
+
+        // status
+        uint8_t connected;
+        char normative_error[15];
+        uint32_t documentation_reference;
+        uint16_t next_packet_id;
 };
 
 #endif
