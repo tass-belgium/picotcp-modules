c1fb85d3338c7479b04b63a17e6235be5f73756c
Maarten Arits <maarten_arits@hotmail.com>
2015-09-23 10:30:19 +0200

    MQTT: simplified iterface and added helper functions

--- pico_mqtt/pico_mqtt.h
+++ pico_mqtt/pico_mqtt.h
@@ -18,6 +18,7 @@ struct pico_mqtt_data
 struct pico_mqtt_message
 {
 	uint8_t header;
+	uint8_t status;
 	uint16_t message_id;
 	struct pico_mqtt_data topic;
 	struct pico_mqtt_data data;
@@ -49,37 +50,65 @@ struct pico_mqtt_message
 * Public Function Prototypes
 **/
 
-// allocate memory for the pico_mqtt object and freeing it
-int pico_mqtt_create(struct pico_mqtt** mqtt, const char* uri, uint32_t* timeout);
-int pico_mqtt_destory(struct pico_mqtt** mqtt);
+/* allocate memory for the pico_mqtt object and free it */
+struct pico_mqtt* pico_mqtt_create(const char* uri);
+void pico_mqtt_destroy(struct pico_mqtt* mqtt);
 
-// connect to the server and disconnect again
-int pico_mqtt_connect(struct pico_mqtt* mqtt, uint32_t* timeout);
+/* getters and setters for the connection options, resenable defaults will be used */
+int pico_mqtt_set_client_id(struct pico_mqtt* mqtt, const struct pico_mqtt_data client_id);
+int pico_mqtt_is_client_id_set(struct pico_mqtt* mqtt);
+struct pico_mqtt_data pico_mqtt_get_client_id(struct pico_mqtt* mqtt);
+
+int pico_mqtt_set_will_message(struct pico_mqtt* mqtt, const struct pico_mqtt_message will_message);
+int pico_mqtt_is_will_message_set(struct pico_mqtt* mqtt);
+struct pico_mqtt_data pico_mqtt_get_will_message(struct pico_mqtt* mqtt);
+
+int pico_mqtt_set_username(struct pico_mqtt* mqtt, const struct pico_mqtt_data username);
+int pico_mqtt_is_username_set(struct pico_mqtt* mqtt);
+struct pico_mqtt_data pico_mqtt_get_username(struct pico_mqtt* mqtt);
+
+int pico_mqtt_set_password(struct pico_mqtt* mqtt, const struct pico_mqtt_data password);
+int pico_mqtt_is_password_set(struct pico_mqtt* mqtt);
+struct pico_mqtt_data pico_mqtt_get_password(struct pico_mqtt* mqtt);
+
+/* connect to the server and disconnect again */
+int pico_mqtt_connect(struct pico_mqtt* mqtt, const uint32_t timeout);
 int pico_mqtt_disconnect(struct pico_mqtt* mqtt);
 
-// ping the server and flush buffer and subscribtions (back to state after create)
-int pico_mqtt_restart(struct pico_mqtt* mqtt, uint32_t* timeout);
-int pico_mqtt_ping(struct pico_mqtt* mqtt, uint32_t* timeout );
+/* ping the server and flush buffer and subscribtions (back to state after create) */
+int pico_mqtt_restart(struct pico_mqtt* mqtt, const uint32_t timeout); /* optional function */
+int pico_mqtt_ping(struct pico_mqtt* mqtt, const uint32_t timeout );
 
-// receive a message or publis a message
-int pico_mqtt_receive(struct pico_mqtt* mqtt, uint32_t* timeout);
-int pico_mqtt_publish(struct pico_mqtt* mqtt, struct pico_mqtt_message* message, uint32_t* timeout);
+/* receive a message or publis a message */
+int pico_mqtt_receive(struct pico_mqtt* mqtt, struct pico_mqtt_message* message, const uint32_t timeout);
+int pico_mqtt_publish(struct pico_mqtt* mqtt, struct pico_mqtt_message* message, const uint32_t timeout);
 
-//subscribe to a topic (or multiple topics) and unsubscribe
-int pico_mqtt_subscribe(struct pico_mqtt* mqtt, const char* topic, const uint8_t quality_of_service, uint32_t* timeout); //return errors, qos is provided in message upon receive
-int pico_mqtt_unsubscribe(struct pico_mqtt* mqtt, const char* topic, uint32_t* timeout);
+/* subscribe to a topic (or multiple topics) and unsubscribe */
+int pico_mqtt_subscribe(struct pico_mqtt* mqtt, const char* topic, const uint8_t quality_of_service, const uint32_t timeout);
+int pico_mqtt_unsubscribe(struct pico_mqtt* mqtt, const char* topic, const uint32_t timeout);
 
 /**
 * Helper Function Prototypes
 **/
 
-// free custom data types
+/* free custom data types */
 int pico_mqtt_destroy_message(struct pico_mqtt_message * message);
 int pico_mqtt_destroy_data(struct pico_mqtt_data * data);
 
-// create and destroy custom data types
-int pico_mqtt_create_data(struct pico_mqtt_data* result, const void* data, const uint32_t length);
-int pico_mqtt_create_message(struct pico_mqtt_message* result, uint8_t header, uint16_t message_id, struct pico_mqtt_data topic, struct pico_mqtt_data data);
+/* create and destroy custom data types */
+struct pico_mqtt_data pico_mqtt_create_data(const void* data, const uint32_t length);
+struct pico_mqtt_message pico_mqtt_create_message(const char* topic, struct pico_mqtt_data data);
+
+/* getters and setters for message options */
+int pico_mqtt_message_set_quality_of_service(struct pico_mqtt_message* message, int quality_of_service);
+int pico_mqtt_message_get_quality_of_service(struct pico_mqtt_message* message);
+
+int pico_mqtt_message_set_retain(struct pico_mqtt_message* message, int quality_of_service);
+int pico_mqtt_message_get_retain(struct pico_mqtt_message* message);
+
+int pico_mqtt_message_is_duplicate_flag_set(struct pico_mqtt_message* message);
+char* pico_mqtt_message_get_topic(struct pico_mqtt_message* message);
+
 
 // get the last error according to the documentation (MQTT specification)
 char* pico_mqtt_get_normative_error( const struct pico_mqtt* mqtt);
